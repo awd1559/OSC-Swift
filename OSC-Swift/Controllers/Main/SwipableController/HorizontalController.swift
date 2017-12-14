c89b291b5853ee96b1064983759466135dd47263
awd1559 <awd1559@gmail.com>
2017-12-14 15:07:58 +0800

    simple HorizontalView SwipableViewController

--- OSC-Swift/Controllers/Main/SwipableController/HorizontalController.swift
+++ OSC-Swift/Controllers/Main/SwipableController/HorizontalController.swift
@@ -6,5 +6,32 @@
 //  Copyright © 2017年 awd. All rights reserved.
 //
 
-import Foundation
-//FIXME: this is HorizontalTableViewController
+import UIKit
+//FIXME: this is HorizonalTableViewController
+
+fileprivate let kHorizonalCellID = "HorizonalCell"
+
+class HorizontalController : UITableViewController {
+    var currentIndex: Int?
+    var controllers: [UIViewController]?
+    var changeIndex: ((Int)->Void)?
+    var scrollView: ((Float, Int, Int)->Void)?
+    var viewDidAppear: ((Int) -> Void)?
+    var viewDidScroll: (()->Void)?
+    
+    init(_ viewControllers:[UIViewController]) {
+        super.init(style: .plain)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func scrollToViewAtIndex(_ index: Int) {
+        
+    }
+    
+    
+    //MARK: - UITableViewDelegate
+    //MARK: - UIScrollViewDelegate
+}
--- OSC-Swift/Controllers/Main/SwipableController/SwipableViewController.swift
+++ OSC-Swift/Controllers/Main/SwipableController/SwipableViewController.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2017年 awd. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SwipableViewController: UIViewController {
+    var viewPager: HorizontalController?
+    var titleBar: TitleBarView?
+    var controllers: [UIViewController]?
+    
+    
+    convenience init(title: String, subTitles:[String], controllers:[UIViewController]) {
+        self.init(title: title, subTitles: subTitles, controllers: controllers, underTabbar: false)
+    }
+    
+    init(title: String, subTitles:[String], controllers:[UIViewController], underTabbar:Bool) {
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func scrollToViewAtIndex(_ index: Int) {
+    
+    }
+}
